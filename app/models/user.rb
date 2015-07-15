2e95e5a4cbd65a9fdacedc1bda9ba6fe7247c686
Alexandre NARBONNE <alexandre.narbonne@gadz.org>
2015-07-15 18:04:12 +0200

    Ajout de la connexion/deconnexion - creation des comptes à la volée

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,37 @@ class User < ActiveRecord::Base
          :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:GadzOrg]
 
-  
+  def update_from_gram
+  	gram_data=GramAccount.find(self.hruid)
+
+  	self.email=gram_data.email
+  	self.firstname=gram_data.firstname
+    self.lastname=gram_data.lastname
+  end
+
+  def self.omniauth(auth_data, signed_in_resource=nil)
+    
+    logger.info "=================================="
+    logger.info "Connexion depuis le CAS uid : "+auth_data[:uid]
+    logger.info "Infos de connection !"
+    logger.info auth_data.inspect
+
+
+    # auth_data : take a look on Users::OmniauthCallbacksController
+    until user = User.find_by_hruid(auth_data[:uid])
+      user = User.new(
+        email: auth_data[:info][:email],
+        password: Devise.friendly_token[0,20],
+        hruid: auth_data[:uid],
+        firstname: auth_data[:extra][:firstname],
+        lastname: auth_data[:extra][:lastname],
+      )
+      user.save!
+    end
+
+    user.update_from_gram
+
+    return user
+  end
 
 end
---
+++ db/migrate/20150715152522_add_basic_infos_to_users.rb
@@ -0,0 +1,7 @@
+class AddBasicInfosToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :hruid, :string
+    add_column :users, :firstname, :string
+    add_column :users, :lastname, :string
+  end
+end
