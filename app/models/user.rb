4fc720ff7bcbd281ce276d47032e36bc5fb3f57a
Alexandre Narbonne <alexandre.narbonne@gadz.org>
2016-01-22 15:58:01 +0100

    Annotate models; refactoring synchro

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,27 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string
+#  last_sign_in_ip        :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  hruid                  :string
+#  firstname              :string
+#  lastname               :string
+#  role_id                :integer
+#  last_gram_sync_at      :datetime
+#
+
 ##
 # A User of the application
 #
@@ -85,7 +109,7 @@ class User < ActiveRecord::Base
   end
 
   def next_sync_allowed_at
-    self.last_gram_sync_at + 5.minutes
+    self.last_gram_sync_at ? self.last_gram_sync_at + 5.minutes : Time.now
   end
 
   ##
