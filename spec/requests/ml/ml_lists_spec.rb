bbe738e1887e7c2904869bedd2d325d89ce48c72
blaked <dorian.becker@gadz.org>
2016-08-21 21:26:28 +0200

    remove wrong test

--- spec/requests/ml/ml_lists_spec.rb
+++ spec/requests/ml/ml_lists_spec.rb
@@ -1,10 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe "Ml::Lists", type: :request do
-  describe "GET /ml_lists" do
-    it "works! (now write some real specs)" do
-      get ml_lists_path
-      expect(response).to have_http_status(200)
-    end
-  end
 end
