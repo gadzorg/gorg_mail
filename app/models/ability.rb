09700523c970f34c746dcc254d5532442fa24b08
blaked <dorian.becker@gadz.org>
2016-09-07 15:52:01 +0200

    fix google app creation ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -48,7 +48,7 @@ class Ability
       can :manage, Ml::ExternalEmail
     end
 
-    can [:read, :sync, :setup, :manage_suscribtion], User, :id => user.id
+    can [:read, :sync, :setup, :manage_suscribtion, :create_google_apps], User, :id => user.id
     can :read_dashboard, User, :id => user.id
     can :manage, EmailRedirectAccount, :user_id => user.id
     can :show, EmailSourceAccount, :user_id => user.id
