b59c2c9868f1098a622906bbbcdfdb9da545a9ff
Alexandre Narbonne <alexandre.narbonne@yahoo.fr>
2016-09-20 16:25:57 +0200

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   private
 
   def after_sign_out_path_for(resource_or_scope)
-    Rails.application.secrets.cas_provider_url ? "#{Rails.application.secrets.cas_provider_url}/cas/logout?service=#{root_url}" : root_url
+    Rails.application.secrets.cas_provider_url ? "https://#{Rails.application.secrets.cas_provider_url}/cas/logout?service=#{root_url}" : root_url
   end
 
   def after_sign_in_path_for(resource_or_scope)
