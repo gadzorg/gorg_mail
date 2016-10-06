b49599b9c99acbfe8c3f2f47e2622ac8d0008972
blaked <dorian.becker@gadz.org>
2016-10-06 10:59:29 +0200

    add maintenance message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   include ConfigurableEngine::ConfigurablesController
 
-  before_filter :check_maintenance_mode
+  before_filter :redirect_if_maintenance_mode
 
   after_filter :prepare_unobtrusive_flash
   private
@@ -47,17 +47,21 @@ class ApplicationController < ActionController::Base
       @lists_joined = user.ml_lists.includes(:users)
     end
 
-    def check_maintenance_mode
-      # caching maintenance state
-      maintenance_mode = Rails.cache.fetch("maintenance_mode", expires_in: 1.minute){ Configurable[:maintenance_mode] }
+    def redirect_if_maintenance_mode
 
-      if maintenance_mode && controller_name != 'devise/sessions' && controller_name != 'static_page' && !current_user.nil?
+
+      if check_maintenance_mode && controller_name != 'devise/sessions' && controller_name != 'static_page' && !current_user.nil?
         unless current_user.has_role?(:admin)  || current_user.has_role?(:support) || Configurable[:allowed_uuid_in_maintenance_mode].split.include?(current_user.uuid)
           cookies.delete(:secureusertokens)
           reset_session
           redirect_to root_path
         end
       end
+    end
+
+    def check_maintenance_mode
+      # caching maintenance state
+      Rails.cache.fetch("maintenance_mode", expires_in: 1.minute){ Configurable[:maintenance_mode] }
     end
 
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,8 @@ class StaticPagesController < ApplicationController
 
   def landing
     if current_user.nil?
+      @maintenance_mode = check_maintenance_mode
+      @maintenance_message = Configurable[:maintenance_message] if @maintenance_mode
       render layout: "landing"
     else
       redirect_to dashboard_path
