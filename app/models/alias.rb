17610f758282b0f9a437c0e9515c03911c401d79
Alexandre Narbonne <alexandre.narbonne@gadz.org>
2016-10-13 14:17:08 +0200

    Put limit on user index

--- app/models/alias.rb
+++ app/models/alias.rb
@@ -3,12 +3,12 @@
 # Table name: aliases
 #
 #  id                      :integer          not null, primary key
-#  email                   :string(255)
-#  redirect                :string(255)
+#  email                   :string
+#  redirect                :string
 #  created_at              :datetime         not null
 #  updated_at              :datetime         not null
-#  alias_type              :string(255)
-#  email_virtual_domain_id :string(255)
+#  alias_type              :string
+#  email_virtual_domain_id :string
 #  expire                  :date
 #  srs_rewrite             :integer
 #  list_id                 :integer
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,25 +3,25 @@
 # Table name: users
 #
 #  id                     :integer          not null, primary key
-#  email                  :string(255)      default("")
-#  encrypted_password     :string(255)      default(""), not null
-#  reset_password_token   :string(255)
+#  email                  :string           default("")
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
 #  reset_password_sent_at :datetime
 #  remember_created_at    :datetime
 #  sign_in_count          :integer          default(0), not null
 #  current_sign_in_at     :datetime
 #  last_sign_in_at        :datetime
-#  current_sign_in_ip     :string(255)
-#  last_sign_in_ip        :string(255)
+#  current_sign_in_ip     :string
+#  last_sign_in_ip        :string
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
-#  hruid                  :string(255)      not null
-#  firstname              :string(255)
-#  lastname               :string(255)
+#  hruid                  :string           not null
+#  firstname              :string
+#  lastname               :string
 #  role_id                :integer
 #  last_gram_sync_at      :datetime
-#  canonical_name         :string(255)
-#  uuid                   :string(255)
+#  canonical_name         :string
+#  uuid                   :string
 #  is_gadz                :boolean
 #
 # Indexes
