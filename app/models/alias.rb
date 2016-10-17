36baf2e9b36d7eba9c58d18b5d96049efe8248b8
blaked <dorian.becker@gadz.org>
2016-10-17 12:44:34 +0200

    add models comments

--- app/models/alias.rb
+++ app/models/alias.rb
@@ -3,12 +3,12 @@
 # Table name: aliases
 #
 #  id                      :integer          not null, primary key
-#  email                   :string
-#  redirect                :string
+#  email                   :string(255)
+#  redirect                :string(255)
 #  created_at              :datetime         not null
 #  updated_at              :datetime         not null
-#  alias_type              :string
-#  email_virtual_domain_id :string
+#  alias_type              :string(255)
+#  email_virtual_domain_id :string(255)
 #  expire                  :date
 #  srs_rewrite             :integer
 #  list_id                 :integer
--- app/models/ml/external_email.rb
+++ app/models/ml/external_email.rb
@@ -4,7 +4,7 @@
 #
 #  id         :integer          not null, primary key
 #  list_id    :integer
-#  email      :string
+#  email      :string(255)
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
