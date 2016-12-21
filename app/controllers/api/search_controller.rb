0420bfe170620e879b48970f1db865137bc94eb2
Alexandre Narbonne <alexandre.narbonne@gadz.org>
2016-12-21 09:43:25 +0100

    Add authentification

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -1,5 +1,7 @@
 class Api::SearchController < ApplicationController
 
+  http_basic_authenticate_with name: Rails.application.secrets.api_user, password: Rails.application.secrets.api_password
+
   def search
     esa=EmailSourceAccount.find_by_full_email(params[:query])
     if esa && esa.user
