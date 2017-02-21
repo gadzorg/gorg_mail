cbf44c749b77638dba40e84feb8b4c27b6df134c
Alexandre Narbonne <alexandre.narbonne@gadz.org>
2017-02-21 10:12:50 +0100

    Change 403 error, update colors for masquerade

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,7 +30,7 @@ class ApplicationController < ActionController::Base
         format.any(:js, :html) {
           store_location_for :user, request.fullpath
           if user_signed_in?
-            render :file => "#{Rails.root}/public/403.html", :status => 403
+            render 'shared/403', :status => 403
           else
             redirect_to new_user_session_path
           end
