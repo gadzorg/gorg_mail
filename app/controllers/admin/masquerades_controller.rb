af022aeb6dad22c93a4ccd958e54a51a531eff26
Alexandre Narbonne <alexandre.narbonne@gadz.org>
2017-02-23 14:38:10 +0100

    GORGMAIL-190 : Add GrAM Sync before masquerading user

--- app/controllers/admin/masquerades_controller.rb
+++ app/controllers/admin/masquerades_controller.rb
@@ -1,7 +1,8 @@
 class Admin::MasqueradesController < Devise::MasqueradesController
   def show
     authorize!(:masquerade, User)
-
+    @user=User.find_by(id: params[:id])
+    @user&&@user.update_from_gram
     super
   end
 end
