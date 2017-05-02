761131e52bcef6eb964ef2067b39c21efe92af6c
Alexandre Narbonne <alexandre.narbonne@gadz.org>
2017-05-02 18:11:28 +0200

    refacto unsubscribe controller

--- app/controllers/unsubscribe_controller.rb
+++ app/controllers/unsubscribe_controller.rb
@@ -1,4 +1,7 @@
 class UnsubscribeController < ApplicationController
+
+  before_action :set_token, only: [:ml_form,:process_unsubscribe]
+
   def email_form
   end
 
@@ -7,25 +10,19 @@ class UnsubscribeController < ApplicationController
   end
 
   def ml_form
-    token=Token.find_by(token: params[:token], scope: UnsubscribeMlService::TOKEN_SCOPE, used_at: nil)
-    if token
-      @mls=UnsubscribeMlService.initialize_from_token(token).mailling_lists
-      @token=params[:token]
-    else
-      render :unknown_token
-    end
+      @mls=UnsubscribeMlService.initialize_from_token(@token).mailling_lists
   end
 
   def process_unsubscribe
+      UnsubscribeMlService.initialize_from_token(@token).perform_unsubscribe(unsubcribe_params)
+  end
 
-    token=Token.find_by(token: params[:token], scope: UnsubscribeMlService::TOKEN_SCOPE, used_at: nil)
-    if token
-      unsubcribe_params=params.require(:unsubscribe)
-      UnsubscribeMlService.initialize_from_token(token).perform_unsubscribe(unsubcribe_params)
-      @token=params[:token]
-    else
-      render :unknown_token
-    end
+  def set_token
+    @token=Token.find_by(token: params.require(:token), scope: UnsubscribeMlService::TOKEN_SCOPE, used_at: nil)
+    render :unknown_token unless @token
+  end
 
+  def unsubcribe_params
+    params.require(:unsubscribe)
   end
 end
