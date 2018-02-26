9a97a0666f98b37d08338aecc82628dc36c07ffc
Guillaume Dufloux <guillaume.dufloux@wopata.com>
2018-02-26 17:39:25 +0100

    Cleanup latest migration

--- db/migrate/20180222171919_add_gadz_centre_and_gadz_proms_to_users.rb
+++ db/migrate/20180222171919_add_gadz_centre_and_gadz_proms_to_users.rb
@@ -2,6 +2,5 @@ class AddGadzCentreAndGadzPromsToUsers < ActiveRecord::Migration
   def change
     add_column :users, :gadz_centre_principal, :string
     add_column :users, :gadz_proms_principale, :string
-    # add_column :users, :subscribed_to_default_mailing_lists_at, :datetime
   end
 end
