8176b9a0f502896de78beeed3561c6d2cfb2acfc
Colin Darie <colin@darie.eu>
2019-09-17 12:04:44 +0200

    Restore configuration for GorgSlackChat

--- config/initializers/gorg_slack_chat.rb
+++ config/initializers/gorg_slack_chat.rb
@@ -1,4 +1,3 @@
-# Disabled until Rails 5.2.3 : there is no compatible version for Rails 5.0/5.1
-# GorgSlackChat.token = Rails.application.secrets.slack_chat_token
-# GorgSlackChat.channel_id = Rails.application.secrets.slack_chat_channel_id
-# GorgSlackChat.app_name = Rails.application.secrets.app_name
+GorgSlackChat.token = Rails.application.secrets.slack_chat_token
+GorgSlackChat.channel_id = Rails.application.secrets.slack_chat_channel_id
+GorgSlackChat.app_name = Rails.application.secrets.app_name
